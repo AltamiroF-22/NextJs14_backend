1c80b2fee30541cdeb8ec1745e29bf8ba893b13a
Altamiro Júnior <124850908+AltamiroF-22@users.noreply.github.com>
2024-05-08 11:48:24 -0300

    Handling PATCH Request

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -1,10 +1,23 @@
 import { comments } from "../data";
 
-export async function GET(
-  _req: Request,
-  { params }: { params: { id: string } }
-) {
-  const comment = comments.find((comment) => comment.id === Number(params.id));
+export interface ParamsProps {
+  params: {
+    id: string;
+  };
+}
 
+export async function GET(_req: Request, { params }: ParamsProps) {
+  const comment = comments.find((comment) => comment.id === Number(params.id));
   return Response.json(comment);
+}
+
+export async function PATCH(req: Request, { params }: ParamsProps) {
+  const body = await req.json();
+  const { text } = body;
+  const index = comments.findIndex(
+    (comment) => comment.id === Number(params.id)
+  );
+  comments[index].text = text;
+
+  return Response.json(comments[index]);
 }
