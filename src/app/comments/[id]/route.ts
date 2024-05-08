8d3b7bb5e8e95d5e7a0a8569aeb3eb4a6158610c
Altamiro Júnior <124850908+AltamiroF-22@users.noreply.github.com>
2024-05-08 12:04:19 -0300

    Handling DELETE Request

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -5,19 +5,33 @@ export interface ParamsProps {
     id: string;
   };
 }
+// utils
+export function findIndex(id: string) {
+  const index = comments.findIndex((comment) => comment.id === Number(id));
+  return index;
+}
+
+export function find(id: string) {
+  const comment = comments.find((comment) => comment.id === Number(id));
+  return comment;
+}
+//------------
 
 export async function GET(_req: Request, { params }: ParamsProps) {
-  const comment = comments.find((comment) => comment.id === Number(params.id));
+  const comment = find(params.id);
   return Response.json(comment);
 }
 
 export async function PATCH(req: Request, { params }: ParamsProps) {
   const body = await req.json();
   const { text } = body;
-  const index = comments.findIndex(
-    (comment) => comment.id === Number(params.id)
-  );
+  const index = findIndex(params.id);
   comments[index].text = text;
-
   return Response.json(comments[index]);
+}
+
+export async function DELETE(_req: Request, { params }: ParamsProps) {
+  const index = findIndex(params.id);
+  const deletedComment = comments.splice(index, 1);
+  return Response.json(deletedComment);
 }
