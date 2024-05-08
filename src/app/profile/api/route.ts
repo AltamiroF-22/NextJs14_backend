8c9e95666d3ee9a714c5601f4b7a9d219b321cac
Altamiro Júnior <124850908+AltamiroF-22@users.noreply.github.com>
2024-05-08 13:29:12 -0300

    Cookies in Route Handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,16 +1,21 @@
 import { NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 
 export async function GET(req: NextRequest) {
   const requestHeaders = new Headers(req.headers);
   const headersList = headers();
-
   console.log(requestHeaders.get("Authorization"));
   console.log(headersList.get("Authorization"));
+
+  const theme = req.cookies.get("theme");
+  cookies().set("resultsperpage", "20");
+  console.log(theme);
+  console.log(cookies().get("resultsperpage"));
 
   return new Response("<h1> profile api data </h1>", {
     headers: {
       "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark",
     },
   });
 }
